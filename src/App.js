b2b4f3c09b2f9caac5fd4faaad750a8e139e641d
Uluc Ozdenvar <ulucozdenvar01@gmail.com>
2021-01-19 12:31:01 -0600

    routes fixed

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
-import React from 'react'
-import {BrowserRouter as Router, Route} from "react-router-dom"
-import "bootstrap/dist/css/bootstrap.min.css"
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import Navbar from "./components/navbar.components";
+import TodoList from "./components/todo-list.components";
+import EditTodo from "./components/edit-todo.components";
+import CreateTodo from "./components/create-todo.components";
+import CreateUser from "./components/create-user.components";
 
 function App() {
   return (
     <Router>
-      <Navbar/>
-      <br/>
-      <Route path = "/" exact component={TodoList}/>
-      <Route path = "/edit/:id" exact component={EditTodo}/>
-      <Route path = "/create" exact component={CreateTodo}/>
-      <Route path = "/user" exact component={CreateTodo}/>
+      <div className="container">
+        <Navbar />
+        <br />
+        <Route path="/" exact component={TodoList} />
+        <Route path="/edit/:id" exact component={EditTodo} />
+        <Route path="/create" exact component={CreateTodo} />
+        <Route path="/user" exact component={CreateUser} />
+      </div>
     </Router>
-
   );
 }
 
