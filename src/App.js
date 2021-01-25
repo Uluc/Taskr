22cd65ee74f9df6910880c08c5dcdbb734456afc
Uluc Ozdenvar <ulucozdenvar01@gmail.com>
2021-01-25 15:05:14 -0600

    private routes added for post login

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import "bootstrap/dist/css/bootstrap.min.css";
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import jwt_decode from "jwt-decode";
+import setAuthToken from "./utils/setAuthToken";
 
-import Navbar from "./components/navbar.components";
-import TodoList from "./components/todo-list.components";
-import EditTodo from "./components/edit-todo.components";
+import { setCurrentUser, logoutUser } from "./actions/authActions";
+import { Provider } from "react-redux";
+import store from "./store";
+
+import Navbar from "./components/navbars/Navbar";
+import TodoList from "./components/TodoList";
 import CreateTodo from "./components/create-todo.components";
-import CreateUser from "./components/create-user.components";
+import EditTodo from "./components/edit-todo.components"
 
+import Landing from "./components/Landing";
+import Register from "./components/auth/Register";
+import Login from "./components/auth/Login";
+import PrivateRoute from "./components/private-route/PrivateRoute";
+import Dashboard from "./components/dashboard/Dashboard";
+
+import "./App.css";
+
+// Check for token to keep user logged in
+if (localStorage.jwtToken) {
+  // Set auth token header auth
+  const token = localStorage.jwtToken;
+  setAuthToken(token);
+  // Decode token and get user info and exp
+  const decoded = jwt_decode(token);
+  // Set user and isAuthenticated
+  store.dispatch(setCurrentUser(decoded));
+  // Check for expired token
+  const currentTime = Date.now() / 1000; // to get in milliseconds
+  if (decoded.exp < currentTime) {
+    // Logout user
+    store.dispatch(logoutUser());
+
+    // Redirect to login
+    window.location.href = "./login";
+  }
+}
 function App() {
   return (
-    <Router>
-      <div className="container">
-        <Navbar />
-        <br />
-        <Route path="/" exact component={TodoList} />
-        <Route path="/edit/:id" exact component={EditTodo} />
-        <Route path="/create" exact component={CreateTodo} />
-        <Route path="/user" exact component={CreateUser} />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+          <Navbar/>
+
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+
+          <Switch>
+
+            <PrivateRoute exact path="/dashboard" component={Dashboard} />
+            <PrivateRoute exact path="/list" component={TodoList} />
+            <PrivateRoute exact path="/edit/:id" component={EditTodo} />
+            <PrivateRoute exact path="/create" component={CreateTodo} />
+          </Switch>
+          
+      </Router>
+    </Provider>
   );
 }
 
