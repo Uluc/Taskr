8ae5e3c02f0378d70fe13b1123330fc2153757d4
Uluc Ozdenvar <ulucozdenvar01@gmail.com>
2021-01-29 16:19:36 -0600

    UI fixed

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,9 +1,57 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { loginUser } from "../../actions/authActions";
 import classnames from "classnames";
+import { WebContainer } from "../../styles/containers";
+
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="textSecondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="#">
+        Taskr
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const useStyles = (theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+});
 
 class Login extends Component {
   constructor() {
@@ -24,7 +72,7 @@ class Login extends Component {
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.auth.isAuthenticated) {
-    this.props.history.push("/dashboard");
+      this.props.history.push("/dashboard");
     }
 
     if (nextProps.errors) {
@@ -51,76 +99,82 @@ class Login extends Component {
 
   render() {
     const { errors } = this.state;
+    const { classes } = this.props;
 
     return (
-      <div className="container">
-        <div style={{ marginTop: "4rem" }} className="row">
-          <div className="col s8 offset-s2">
-            <Link to="/" className="btn-flat waves-effect">
-              <i className="material-icons left">keyboard_backspace</i> Back to
-              home
-            </Link>
-            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-              <h4>
-                <b>Login</b>
-              </h4>
-              <p className="grey-text text-darken-1">
-                Don't have an account? <Link to="/register">Register</Link>
-              </p>
-            </div>
-            <form noValidate onSubmit={this.onSubmit}>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.email}
-                  error={errors.email}
-                  id="email"
-                  type="email"
-                  className={classnames("", {
-                    invalid: errors.email || errors.emailnotfound,
-                  })}
-                />
-                <label htmlFor="email">Email</label>
-                <span className="red-text">
-                  {errors.email}
-                  {errors.emailnotfound}
-                </span>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password}
-                  error={errors.password}
-                  id="password"
-                  type="password"
-                  className={classnames("", {
-                    invalid: errors.password || errors.passwordincorrect,
-                  })}
-                />
-                <label htmlFor="password">Password</label>
-                <span className="red-text">
-                  {errors.password}
-                  {errors.passwordincorrect}
-                </span>
-              </div>
-              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                <button
-                  style={{
-                    width: "150px",
-                    borderRadius: "3px",
-                    letterSpacing: "1.5px",
-                    marginTop: "1rem",
-                  }}
-                  type="submit"
-                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                >
-                  Login
-                </button>
-              </div>
-            </form>
-          </div>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+          <form className={classes.form} noValidate onSubmit={this.onSubmit}>
+            <TextField
+              onChange={this.onChange}
+              value={this.state.email}
+              error={errors.email}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              autoFocus
+            />
+            <span className="red">
+              {errors.email}
+              {errors.emailnotfound}
+            </span>
+            <TextField
+              onChange={this.onChange}
+              value={this.state.password}
+              error={errors.password}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+            <span className="red">
+              {errors.password}
+              {errors.passwordincorrect}
+            </span>
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign In
+            </Button>
+            <Grid container>
+              <Grid item xs>
+                <Link href="#" variant="body2">
+                  Forgot password?
+                </Link>
+              </Grid>
+              <Grid item>
+                <Link href="/register" variant="body2">
+                  {"Don't have an account? Sign Up"}
+                </Link>
+              </Grid>
+            </Grid>
+          </form>
         </div>
-      </div>
+        <Box mt={8}>
+          <Copyright />
+        </Box>
+      </Container>
     );
   }
 }
@@ -136,4 +190,6 @@ const mapStateToProps = (state) => ({
   errors: state.errors,
 });
 
-export default connect(mapStateToProps, { loginUser })(Login);
+export default connect(mapStateToProps, { loginUser })(
+  withStyles(useStyles)(Login)
+);
--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,9 +1,46 @@
 import React, { Component } from "react";
-import { Link, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { registerUser } from "../../actions/authActions";
 import classnames from "classnames";
+
+import Avatar from "@material-ui/core/Avatar";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Checkbox from "@material-ui/core/Checkbox";
+import Link from "@material-ui/core/Link";
+import { withStyles } from "@material-ui/core/styles";
+import Grid from "@material-ui/core/Grid";
+import Box from "@material-ui/core/Box";
+import LockOutlinedIcon from "@material-ui/icons/LockOutlined";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import Container from "@material-ui/core/Container";
+
+const useStyles = (theme) => ({
+  paper: {
+    marginTop: theme.spacing(8),
+    display: "flex",
+    flexDirection: "column",
+    alignItems: "center",
+  },
+  avatar: {
+    margin: theme.spacing(1),
+    backgroundColor: theme.palette.secondary.main,
+  },
+  form: {
+    width: "100%", // Fix IE 11 issue.
+    marginTop: theme.spacing(1),
+  },
+  submit: {
+    margin: theme.spacing(3, 0, 2),
+  },
+});
+
+
 class Register extends Component {
   constructor() {
     super();
@@ -23,7 +60,6 @@ class Register extends Component {
       this.props.history.push("/dashboard");
     }
   }
-
   componentWillReceiveProps(nextProps) {
     if (nextProps.errors) {
       this.setState({
@@ -47,111 +83,202 @@ class Register extends Component {
   };
   render() {
     const { errors } = this.state;
+    const { classes } = this.props;
+
     return (
-      <div className="container">
-        <div className="row">
-          <div className="col s8 offset-s2">
-            <Link to="/" className="btn-flat waves-effect">
-              <i className="material-icons left">keyboard_backspace</i> Back to
-              home
-            </Link>
-            <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-              <h4>
-                <b>Register</b>
-              </h4>
-              <p className="grey-text text-darken-1">
-                Already have an account? <Link to="/login">Log in</Link>
-              </p>
-            </div>
-            <form noValidate onSubmit={this.onSubmit}>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.name}
-                  error={errors.firstName}
-                  id="firstName"
-                  type="text"
-                  className={classnames("", {
-                    invalid: errors.firstName,
-                  })}
-                />
-                <label htmlFor="firstName">First Name</label>
-                <span className="red-text">{errors.firstName}</span>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.name}
-                  error={errors.name}
-                  id="lastName"
-                  type="text"
-                  className={classnames("", {
-                    invalid: errors.lastName,
-                  })}
-                />
-                <label htmlFor="lastName">Last Name</label>
-                <span className="red-text">{errors.lastName}</span>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.email}
-                  error={errors.email}
-                  id="email"
-                  type="email"
-                  className={classnames("", {
-                    invalid: errors.email,
-                  })}
-                />
-                <label htmlFor="email">Email</label>
-                <span className="red-text">{errors.email}</span>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password}
-                  error={errors.password}
-                  id="password"
-                  type="password"
-                  className={classnames("", {
-                    invalid: errors.password,
-                  })}
-                />
-                <label htmlFor="password">Password</label>
-                <span className="red-text">{errors.password}</span>
-              </div>
-              <div className="input-field col s12">
-                <input
-                  onChange={this.onChange}
-                  value={this.state.password2}
-                  error={errors.password2}
-                  id="password2"
-                  type="password"
-                  className={classnames("", {
-                    invalid: errors.password2,
-                  })}
-                />
-                <label htmlFor="password2">Confirm Password</label>
-                <span className="red-text">{errors.password2}</span>
-              </div>
-              <div className="col s12" style={{ paddingLeft: "11.250px" }}>
-                <button
-                  style={{
-                    width: "150px",
-                    borderRadius: "3px",
-                    letterSpacing: "1.5px",
-                    marginTop: "1rem",
-                  }}
-                  type="submit"
-                  className="btn btn-large waves-effect waves-light hoverable blue accent-3"
-                >
-                  Sign up
-                </button>
-              </div>
-            </form>
-          </div>
+      <Container component="main" maxWidth="xs">
+        <CssBaseline />
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Register
+          </Typography>
+          <form className={classes.form} noValidate onSubmit={this.onSubmit}>
+
+          <TextField
+              onChange={this.onChange}
+              value={this.state.firstName}
+              error={errors.firstName}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="firstName"
+              label="First Name"
+              name="firstName"
+              autoFocus
+            />
+             <TextField
+              onChange={this.onChange}
+              value={this.state.lastName}
+              error={errors.email}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="lastName"
+              label="Last Name"
+              name="lastName"
+              autoFocus
+            />
+            <TextField
+              onChange={this.onChange}
+              value={this.state.email}
+              error={errors.email}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoFocus
+            />
+            <TextField
+              onChange={this.onChange}
+              value={this.state.password}
+              error={errors.password}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              name="password"
+              label="Password"
+              type="password"
+              id="password"
+              autoComplete="current-password"
+            />
+             <TextField
+              onChange={this.onChange}
+              value={this.state.password2}
+              error={errors.password2}
+              variant="outlined"
+              margin="normal"
+              required
+              fullWidth
+              id="password2"
+              label="Password"
+              name="password2"
+              autoFocus
+            />
+            <Button
+              type="submit"
+              fullWidth
+              variant="contained"
+              color="primary"
+              className={classes.submit}
+            >
+              Sign Up
+            </Button>
+          </form>
         </div>
-      </div>
+      </Container>
+      // <div className="container">
+      //   <div className="row">
+      //     <div className="col s8 offset-s2">
+      //       <Link to="/" className="btn-flat waves-effect">
+      //         <i className="material-icons left">keyboard_backspace</i> Back to
+      //         home
+      //       </Link>
+      //       <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+      //         <h4>
+      //           <b>Register</b>
+      //         </h4>
+      //         <p className="grey-text text-darken-1">
+      //           Already have an account? <Link to="/login">Log in</Link>
+      //         </p>
+      //       </div>
+      //       <form noValidate onSubmit={this.onSubmit}>
+      //         <div className="input-field col s12">
+      //           <input
+      //             onChange={this.onChange}
+      //             value={this.state.name}
+      //             error={errors.firstName}
+      //             id="firstName"
+      //             type="text"
+      //             className={classnames("", {
+      //               invalid: errors.firstName,
+      //             })}
+      //           />
+      //           <label htmlFor="firstName">First Name</label>
+      //           <span className="red-text">{errors.firstName}</span>
+      //         </div>
+      //         <div className="input-field col s12">
+      //           <input
+      //             onChange={this.onChange}
+      //             value={this.state.name}
+      //             error={errors.name}
+      //             id="lastName"
+      //             type="text"
+      //             className={classnames("", {
+      //               invalid: errors.lastName,
+      //             })}
+      //           />
+      //           <label htmlFor="lastName">Last Name</label>
+      //           <span className="red-text">{errors.lastName}</span>
+      //         </div>
+      //         <div className="input-field col s12">
+      //           <input
+      //             onChange={this.onChange}
+      //             value={this.state.email}
+      //             error={errors.email}
+      //             id="email"
+      //             type="email"
+      //             className={classnames("", {
+      //               invalid: errors.email,
+      //             })}
+      //           />
+      //           <label htmlFor="email">Email</label>
+      //           <span className="red-text">{errors.email}</span>
+      //         </div>
+      //         <div className="input-field col s12">
+      //           <input
+      //             onChange={this.onChange}
+      //             value={this.state.password}
+      //             error={errors.password}
+      //             id="password"
+      //             type="password"
+      //             className={classnames("", {
+      //               invalid: errors.password,
+      //             })}
+      //           />
+      //           <label htmlFor="password">Password</label>
+      //           <span className="red-text">{errors.password}</span>
+      //         </div>
+      //         <div className="input-field col s12">
+      //           <input
+      //             onChange={this.onChange}
+      //             value={this.state.password2}
+      //             error={errors.password2}
+      //             id="password2"
+      //             type="password"
+      //             className={classnames("", {
+      //               invalid: errors.password2,
+      //             })}
+      //           />
+      //           <label htmlFor="password2">Confirm Password</label>
+      //           <span className="red-text">{errors.password2}</span>
+      //         </div>
+      //         <div className="col s12" style={{ paddingLeft: "11.250px" }}>
+      //           <button
+      //             style={{
+      //               width: "150px",
+      //               borderRadius: "3px",
+      //               letterSpacing: "1.5px",
+      //               marginTop: "1rem",
+      //             }}
+      //             type="submit"
+      //             className="btn btn-large waves-effect waves-light hoverable blue accent-3"
+      //           >
+      //             Sign up
+      //           </button>
+      //         </div>
+      //       </form>
+      //     </div>
+      //   </div>
+      // </div>
     );
   }
 }
@@ -164,4 +291,4 @@ const mapStateToProps = (state) => ({
   auth: state.auth,
   errors: state.errors,
 });
-export default connect(mapStateToProps, { registerUser })(withRouter(Register));
+export default connect(mapStateToProps, { registerUser })(withStyles(useStyles)((withRouter(Register))));
